cab2e62a8e5a77100b7a899fe37e2c4623c178a2
laxmi Tiruvuri <latiruvu@microsoft.com>
2019-02-20 14:05:54 -0800

    Plurals stuff and not oop

---
+++ oop/oop/Program.cs
@@ -0,0 +1,38 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace oop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // The code provided will print ‘Hello World’ to the console.
+            // Press Ctrl+F5 (or go to Debug > Start Without Debugging) to run your app.
+            //pluralsight
+            if(DateTime.Now.DayOfWeek == DayOfWeek.Wednesday)
+            {
+                Console.WriteLine("Another case of the Wednesday!");
+            }
+            Console.WriteLine("Your name:");
+            string name = Console.ReadLine();
+            Console.WriteLine("Hello, " + name);
+            int hoursOfSleep = int.Parse(Console.ReadLine());
+
+            if(hoursOfSleep > 8)
+            {
+                Console.WriteLine("you are well rested");
+            } 
+            else
+            {
+                Console.WriteLine("you need more sleep");
+            }
+            Console.ReadKey();
+
+            // Go to http://aka.ms/dotnet-get-started-console to continue learning how to build a console app! 
+        }
+    }
+}
