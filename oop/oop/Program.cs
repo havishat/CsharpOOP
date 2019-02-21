4d375db7946c46e1b45da9581dac06bd010abd5a
laxmi Tiruvuri <latiruvu@microsoft.com>
2019-02-21 09:56:20 -0800

    finished grade book classes.

--- oop/oop/Program.cs
+++ oop/oop/Program.cs
@@ -13,25 +13,38 @@ namespace oop
             // The code provided will print ‘Hello World’ to the console.
             // Press Ctrl+F5 (or go to Debug > Start Without Debugging) to run your app.
             //pluralsight
-            if(DateTime.Now.DayOfWeek == DayOfWeek.Wednesday)
-            {
-                Console.WriteLine("Another case of the Wednesday!");
-            }
-            Console.WriteLine("Your name:");
-            string name = Console.ReadLine();
-            Console.WriteLine("How many hours of sleep did you get last night?");
-            int hoursOfSleep = int.Parse(Console.ReadLine());
+            //if(DateTime.Now.DayOfWeek == DayOfWeek.Wednesday)
+            //{
+            //    Console.WriteLine("Another case of the Wednesday!");
+            //}
+            //Console.WriteLine("Your name:");
+            //string name = Console.ReadLine();
+            //Console.WriteLine("How many hours of sleep did you get last night?");
+            //int hoursOfSleep = int.Parse(Console.ReadLine());
 
-            Console.WriteLine("Hello, " + name);
-            
-            if(hoursOfSleep > 8)
-            {
-                Console.WriteLine("you are well rested");
-            } 
-            else
-            {
-                Console.WriteLine("you need more sleep");
-            }
+            //Console.WriteLine("Hello, " + name);
+
+            //if(hoursOfSleep > 8)
+            //{
+            //    Console.WriteLine("you are well rested");
+            //} 
+            ////else
+            ////{
+            ////    Console.WriteLine("you need more sleep");
+            ////}
+            ///
+
+            GradeBook book = new GradeBook();
+            book.AddGrade(91);
+            book.AddGrade(89.5f);
+            book.AddGrade(75);
+
+            GradeStatistics stats = book.ComputeStatistics();
+            Console.WriteLine(stats.AverageGrade);
+            Console.WriteLine(stats.HighestGrade);
+            Console.WriteLine(stats.LowestGrade);
+
+
             Console.ReadKey();
 
             // Go to http://aka.ms/dotnet-get-started-console to continue learning how to build a console app! 
